aa16561ecf56f522f5ae3efb770b9bfe0927e77b
Bahtyyar Abayev <92421009+bahtyyarabayev@users.noreply.github.com>
2023-05-12 13:50:33 +0300

    Oto Galeri Otomasyon (Visual Studio)
    Müşteri girişi, araç satış, araç kiralama, araç teslim gibi farklı menülerden oluşan Otomasyon uygulaması

---
+++ otogaleriotomasyonu/otogaleriotomasyonu/oto_galeri/oto_kiralama_otomasyonu/arac_satis.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace oto_kiralama_otomasyonu
+{
+    public partial class arac_satis : Form
+    {
+        public arac_satis()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection("Data Source=HP-Bilgisayar;Initial Catalog=pizza;Integrated Security=True");
+        DataTable dt = new DataTable();
+        private void arac_satis_Load(object sender, EventArgs e)
+        {
+            musteridoldur();
+            uygunaracdoldur();
+            doldur();
+        }
+        void uygunaracdoldur()
+        {
+            comboBox2.Items.Clear();
+            if (baglanti.State == ConnectionState.Closed)
+            {
+                baglanti.Open();
+            }
+            SqlCommand komut = new SqlCommand("select plaka from arac where satis_durum='" + "Satışa Uygun" + "'", baglanti);
+            SqlDataReader oku = komut.ExecuteReader();
+            while (oku.Read())
+            {
+                comboBox2.Items.Add(oku["plaka"].ToString());
+            }
+            baglanti.Close();
+        }
+        void musteridoldur()
+        {
+            comboBox1.Items.Clear();
+            if (baglanti.State == ConnectionState.Closed)
+            {
+                baglanti.Open();
+            }
+            SqlCommand komut = new SqlCommand("select tc from musteri", baglanti);
+            SqlDataReader oku = komut.ExecuteReader();
+            while (oku.Read())
+            {
+                comboBox1.Items.Add(oku["tc"].ToString());
+            }
+            baglanti.Close();
+        }
+        void doldur()
+        {
+            try
+            {
+                if (baglanti.State == ConnectionState.Closed)
+                {
+                    baglanti.Open();
+                }
+                dt.Clear();
+                SqlDataAdapter listele = new SqlDataAdapter("select * from arac_satis", baglanti);
+                listele.Fill(dt);
+                dataGridView1.DataSource = dt;
+                listele.Dispose();
+                dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                dataGridView1.Columns[0].Visible = false;
+                dataGridView1.RowHeadersVisible = false;
+                dataGridView1.Columns[1].Width = 80;
+                dataGridView1.Columns[2].Width = 120;
+                dataGridView1.Columns[3].Width = 77;
+                dataGridView1.Columns[4].Width = 76;
+                dataGridView1.Columns[5].Width = 76;
+
+                baglanti.Close();
+
+                comboBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+                textBox5.Text = "";
+
+                comboBox1.Text = "";
+            }
+            catch (Exception hata)
+            {
+                MessageBox.Show(hata.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand komut = new SqlCommand("insert into arac_satis(tc,plaka,alis_tarihi,veris_tarihi,ucret) values('" + comboBox1.Text + "','" + comboBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "','" + textBox5.Text + "')", baglanti);
+                //
+                // 
+                if (baglanti.State == ConnectionState.Closed)
+                {
+                    baglanti.Open();
+                }
+
+                komut.ExecuteNonQuery();
+
+                try
+                {
+                    if (baglanti.State == ConnectionState.Closed)
+                    {
+                        baglanti.Open();
+                    }
+
+                  
+                    SqlCommand sil = new SqlCommand("delete from arac where plaka='" + comboBox2.Text + "'", baglanti);
+                    sil.ExecuteNonQuery();
+                    uygunaracdoldur();
+                    doldur();
+                }
+                catch 
+                {
+
+                    ;
+                }
+                MessageBox.Show("Satış İşlemi İşleminiz Başarılı");
+                baglanti.Close();
+
+                doldur();
+            }
+            catch (Exception hata)
+            {
+
+                MessageBox.Show(hata.Message);
+            }
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+            if (baglanti.State == ConnectionState.Closed)
+            {
+                baglanti.Open();
+            }
+            dt.Clear();
+
+            SqlDataAdapter adtr = new SqlDataAdapter("select * From arac_satis where tc like'%" + textBox7.Text + "%'or plaka like '%" + textBox7.Text + "%'or alis_tarihi like '%" + textBox7.Text + "%'or veris_tarihi like '%" + textBox7.Text + "%'or ucret like '%" + textBox7.Text + "%'", baglanti);
+            adtr.Fill(dt);
+            dataGridView1.DataSource = dt;
+            adtr.Dispose();
+
+
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.Columns[0].Visible = false;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.Columns[1].Width = 80;
+            dataGridView1.Columns[2].Width = 120;
+            dataGridView1.Columns[3].Width = 77;
+            dataGridView1.Columns[4].Width = 76;
+            dataGridView1.Columns[5].Width = 76;
+
+            baglanti.Close();
+
+            comboBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+
+            comboBox1.Text = "";
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            textBox3.Text = dateTimePicker1.Value.ToShortDateString();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            textBox4.Text = dateTimePicker2.Value.ToShortDateString();
+            
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            comboBox1.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            comboBox2.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            textBox3.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            textBox4.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            textBox5.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            menu menu = new menu();
+            menu.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult cevap;
+            cevap = MessageBox.Show("Kaydı silmek istediğinizden eminmisiniz", "Uyarı", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (cevap == DialogResult.Yes && dataGridView1.CurrentRow.Cells[0].Value.ToString().Trim() != "")
+            {
+                try
+                {
+
+                    if (baglanti.State == ConnectionState.Closed)
+                    {
+                        baglanti.Open();
+                    }
+                    SqlCommand sil = new SqlCommand("delete from arac_satis where id='" + dataGridView1.CurrentRow.Cells[0].Value.ToString() + "'", baglanti);
+                    sil.ExecuteNonQuery();
+                    uygunaracdoldur();
+                    musteridoldur();
+                    MessageBox.Show("Silme İşleminiz Başarılı");
+                    doldur();
+                    baglanti.Close();
+
+                }
+                catch
+                {
+
+                    ;
+                }
+            }
+    }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand("update arac_satis set tc='" + comboBox1.Text + "',plaka='" + comboBox2.Text + "',alis_tarihi='" + textBox3.Text + "',veris_tarihi='" + textBox4.Text + "',ucret='" + textBox5.Text + "' where id='" + dataGridView1.CurrentRow.Cells[0].Value.ToString() + "'", baglanti);
+            // 
+            if (baglanti.State == ConnectionState.Closed)
+            {
+                baglanti.Open();
+            }
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Güncelleme İşlemi Başarılı");
+            doldur();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ otogaleriotomasyonu/otogaleriotomasyonu/oto_galeri/oto_kiralama_otomasyonu/musteri_islemleri.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace oto_kiralama_otomasyonu
+{
+    public partial class musteri_islemleri : Form
+    {
+        public musteri_islemleri()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection("Data Source=HP-Bilgisayar;Initial Catalog=pizza;Integrated Security=True");
+        DataTable dt = new DataTable();
+       
+        private void musteri_islemleri_Load(object sender, EventArgs e)
+        {
+            doldur();
+        }
+
+        void doldur()
+        {
+            try
+            {
+                if (baglanti.State == ConnectionState.Closed)
+                {
+                    baglanti.Open();
+                }
+                dt.Clear();
+                SqlDataAdapter listele = new SqlDataAdapter("select * from musteri ", baglanti);
+                listele.Fill(dt);
+                dataGridView1.DataSource = dt;
+                listele.Dispose();
+                dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                dataGridView1.Columns[0].Visible = false;
+                dataGridView1.RowHeadersVisible = false;
+                dataGridView1.Columns[1].Width = 80;
+                dataGridView1.Columns[2].Width = 120;
+                dataGridView1.Columns[3].Width = 77;
+                dataGridView1.Columns[4].Width = 76;
+                dataGridView1.Columns[5].Width = 76;
+                dataGridView1.Columns[6].Width = 80;
+                baglanti.Close();
+                maskedTextBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+                textBox5.Text = "";
+                textBox6.Text = "";
+                textBox8.Text = "";
+                textBox9.Text = "";
+                comboBox1.Text= "";
+            }
+            catch (Exception hata)
+            {
+                MessageBox.Show(hata.Message);
+            }
+          
+
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            menu menu = new menu();
+            menu.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (maskedTextBox1.Text.Length != 11)
+            {
+                MessageBox.Show("hata!!! TC numarası giriş Formatınız Yanlıştır.");
+            }
+            else
+            {
+
+                try
+            {
+                SqlCommand komut = new SqlCommand("insert into musteri(tc,adi_soyadi,cinsiyet,telefon,dogum_tarihi,ehliyet_no,sifre,gizli_soru,yaniti) values('" + maskedTextBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "','" + textBox5.Text + "','" + textBox6.Text + "','"+ textBox8.Text+ "','"+comboBox1.Text+ "','"+textBox9.Text+"')", baglanti);
+                //
+                // 
+                if (baglanti.State == ConnectionState.Closed)
+                {
+                    baglanti.Open();
+                }
+
+                komut.ExecuteNonQuery();
+                MessageBox.Show("Ekleme İşleminiz Başarılı");
+                baglanti.Close();
+              
+                doldur();
+            }
+            catch (Exception hata)
+            {
+
+                MessageBox.Show(hata.Message);
+                }
+            }
+
+            //
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            maskedTextBox1.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBox2.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            textBox3.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            textBox4.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            textBox5.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+            textBox6.Text = dataGridView1.CurrentRow.Cells[6].Value.ToString();
+            textBox8.Text = dataGridView1.CurrentRow.Cells[7].Value.ToString();
+            comboBox1.Text = dataGridView1.CurrentRow.Cells[8].Value.ToString();
+            textBox9.Text = dataGridView1.CurrentRow.Cells[9].Value.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult cevap;
+            cevap = MessageBox.Show("Kaydı silmek istediğinizden eminmisiniz", "Uyarı", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (cevap == DialogResult.Yes && dataGridView1.CurrentRow.Cells[0].Value.ToString().Trim() != "")
+            {
+                try
+                {
+                    
+                    if (baglanti.State==ConnectionState.Closed)
+                    {
+                        baglanti.Open();
+                    }
+                    SqlCommand sil = new SqlCommand("delete from musteri where id='" + dataGridView1.CurrentRow.Cells[0].Value.ToString() + "'", baglanti);
+                    sil.ExecuteNonQuery();
+                    MessageBox.Show("Silme İşleminiz Başarılı");
+                    doldur();
+                    baglanti.Close();
+
+                }
+                catch 
+                {
+
+   ;
+                }
+
+            }
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+          
+                dt.Clear();
+
+                SqlDataAdapter adtr = new SqlDataAdapter("select * From musteri where tc like'%" + textBox7.Text + "%'or adi_soyadi like '%" + textBox7.Text + "%'or cinsiyet like '%" + textBox7.Text + "%'or telefon like '%" + textBox7.Text + "%'or dogum_tarihi like '%" + textBox7.Text + "%'", baglanti);
+                adtr.Fill(dt);
+                dataGridView1.DataSource = dt;
+                adtr.Dispose();
+
+
+                dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                dataGridView1.Columns[0].Visible = false;
+                dataGridView1.RowHeadersVisible = false;
+                dataGridView1.Columns[1].Width = 80;
+                dataGridView1.Columns[2].Width = 120;
+                dataGridView1.Columns[3].Width = 77;
+                dataGridView1.Columns[4].Width = 76;
+                dataGridView1.Columns[5].Width = 76;
+                dataGridView1.Columns[6].Width = 80;
+                
+
+
+
+           
+       
+    }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            baglanti.Open();
+            SqlCommand adtr = new SqlCommand("UPDATE musteri set tc='"+maskedTextBox1.Text+"',adi_soyadi='"+textBox2.Text+"',cinsiyet='"+textBox3.Text+"',telefon='"+textBox4.Text+"',dogum_tarihi='"+textBox5.Text+"',ehliyet_no='"+textBox6.Text+"'where id='"+ dataGridView1.CurrentRow.Cells[0].Value.ToString() + "'", baglanti);
+            adtr.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Güncelleme İşlemi Başarılı");
+            doldur();
+        }
+    }
+}
